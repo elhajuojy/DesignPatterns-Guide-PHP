a747a915b016e6f7925ed2af4304e97859eb8d05
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-11 11:12:33 +0100

    singleton example create one instance of the class

--- Creational design pattern/singleton.php
+++ Creational design pattern/singleton.php
@@ -1,3 +1,47 @@
 <?php 
 
-echo "singleton design pattern";
+
+class Singleton
+{
+    private static $instance = null;
+    private $name = null;
+    private $count = 0;
+
+    private function __construct()
+    {
+        $this->name = "Singleton";
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new Singleton();
+        }
+        return self::$instance;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    public function addOne(){
+        $this->count++;
+    }
+}
+
+$singleton = Singleton::getInstance();
+$singleton1 = singleton::getInstance();
+
+//2 object adding 1 to count
+$singleton->addOne();
+$singleton1->addOne();
+
+
+//the count will be 2 because the 2 object are the same instance of the class
+echo $singleton1->getCount();
