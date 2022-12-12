0a9116572ffd726230df48104f377cd043d90088
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-12 14:23:25 +0100

    strategy pattern 💖

---
+++ Strategy Pattern/Readme.md
@@ -0,0 +1,9 @@
+# strategy pattern
+
+he strategy pattern (also known as the policy pattern) is a software design pattern that enables an algorithm’s behavior to be selected at runtime. The strategy pattern
+
+defines a family of algorithms,
+encapsulates each algorithm, and
+makes the algorithms interchangeable within that family.”
+**Class Diagram:**
+![Class Diagram:](https://media.geeksforgeeks.org/wp-content/uploads/classinh.jpg)
---
+++ Strategy Pattern/StrategyPattern.php
@@ -0,0 +1,57 @@
+<?php
+
+
+// Path: Strategy Pattern\StrategyPattern.php
+
+// Compare this snippet from Creational design pattern\index.php:
+
+class Client{
+    private  $strategy;
+
+    public function __construct(){
+        // $this->strategy = $strategy;
+    }
+    
+    public function runOpreation($num1, $num2){
+        return $this->strategy->doOperation($num1, $num2);
+    }
+
+    public function setStrategy($strategy){
+        $this->strategy = $strategy;
+    }
+
+    public function doOperation($num1, $num2){
+        return $this->strategy->doOperation($num1, $num2);
+    }
+}
+
+// Path: Strategy Pattern\StrategyPattern.php
+interface Strategy{
+    public function doOperation($num1, $num2);
+}
+
+class OperationAdd implements Strategy{
+    public function doOperation($num1, $num2){
+        return $num1 + $num2;
+    }
+}
+
+class OperationSubstract implements Strategy{
+    public function doOperation($num1, $num2){
+        return $num1 - $num2;
+    }
+}
+
+$Client1 = new Client();
+$Client1->setStrategy(new OperationAdd());
+echo  $Client1->runOpreation(10, 5);
+
+echo "\n";
+$Client2 = new Client();
+$Client2->setStrategy(new OperationSubstract());
+echo  $Client2->runOpreation(10, 5);
+
+
+// output:
+// 15
+// 5
